fba7cb2b6e1d2deb61118ccf85587c46228b45ab
Eburciaga020 <edgar_3041230332@utd.edu.mx>
2024-05-17 11:37:47 -0600

    SI

--- Parcial1_2A/5_Estructuras_Control_Ciclicas/While.py
+++ Parcial1_2A/5_Estructuras_Control_Ciclicas/While.py
@@ -1,22 +1,26 @@
-# Ciclo for estructura interativa que se ejecuta x veces
+# Ciclo while estructura interativa que se ejecuta x veces siempre y cuando la condicion se cumla y se seguira ejecutado tanta veces como sea True la condicion
 
 #sintaxis
-#for (Variables) in (elementos_interable) (lista, rango, etc):
+#while condicion:
     #bloque de intrucciones
 
 #Ejemplo 1 crear un programa que imprima 5 veces el @
 
+contador=1
 
-for i in range(1,6):
+while contador<=5:
     print("@")
+    contador+=1
 
 #Ejemplo 2 crear un programa que imprima los numeros del 1 al 5 y los sume y que al final imprima la suma 
 
+contador=1
 suma=0
 
-for contador in range(1,6):
+while contador<=5:
     print(contador)
     suma+=contador
+    contador+=1
 
 print(f"La suma es: {suma}")
 
@@ -24,8 +28,10 @@ print(f"La suma es: {suma}")
 
 tabla=int(input("Ingresa un numero para calcular su tabla de multiplicar: ")) 
 
+i=1
 multi=0
 
-for i in range(1,11):
+while i<=10:
     multi=i*tabla
     print(f"{tabla} X {i} = {multi}")
+    i+=1
