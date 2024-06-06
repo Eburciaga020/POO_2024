b368ac646f307d55fc9acabb5bc76ac3b7d8b41e
Eburciaga020 <edgar_3041230332@utd.edu.mx>
2024-06-06 09:13:25 -0600

    si

--- Parcial1_2A/8_Listas/Listas.py
+++ Parcial1_2A/8_Listas/Listas.py
@@ -28,10 +28,29 @@ while i<len(numeros):
     print(numeros[i])
     i+=1
 
-# Ejemplo 2 Crea una lista de palabas, posteriormente ingresar una palabra 
-# para buscar la coincidencia en lista E indicar si aparece la palabra y en que
-# posicion en caso contrario indicar una sola vez si no lo encuentra
+"""
+Ejemplo 2 Crea una lista de palabas, posteriormente ingresar una palabra 
+para buscar la coincidencia en lista E indicar si aparece la palabra y en que
+posicion en caso contrario indicar una sola vez si no lo encuentra
+"""
 
 palabras=["hola","2024","10.23","True"]
 
-palabras_buscar=input("Ingresa la palabra a buscar")
+palabras_buscar=input("Ingresa la palabra a buscar: ")
+
+noencontro=True
+
+# for i in palabras:
+#     if palabras_buscar==i:
+        # print(f"Encontre la palabra {palabras_buscar}, en la posicion: {palabras.index(i)}")
+        # noencontro=False
+
+while i<len(palabras):
+   if palabras_buscar==palabras[i]:
+      print(f"Encontre la palabra {palabras_buscar}, en la posicion: {palabras.index(i)}")
+      noencontro=False
+
+
+
+if noencontro:
+ print(f"No se encontro la palabra dentro de la lista")
